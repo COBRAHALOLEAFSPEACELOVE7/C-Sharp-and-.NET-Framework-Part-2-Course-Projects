bf2283318553e1b596d15323bc125861f4b3ed8c
COBRAHALOLEAFSPEACELOVE7 <129617008+COBRAHALOLEAFSPEACELOVE7@users.noreply.github.com>
2023-09-27 20:45:46 -0400

    Add files via upload

---
+++ Method Class Assignment/Hockey777.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Hockey777
+    {
+        //Created or made a method that takes two numbers or intergers as parameters :)
+        public int Hockey(int C, int L)
+        {
+            //Added C + L and then showed the result
+            int result = C + L;
+            return result;
+            
+        }
+    }
+}
