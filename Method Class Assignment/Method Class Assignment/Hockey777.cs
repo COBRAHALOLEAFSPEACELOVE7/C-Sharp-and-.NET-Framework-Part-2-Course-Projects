98217dde265cedd4c50619a06b568a4e39ec929d
COBRAHALOLEAFSPEACELOVE7 <129617008+COBRAHALOLEAFSPEACELOVE7@users.noreply.github.com>
2023-10-01 23:50:44 -0400

    Add files via upload

---
+++ Method Class Assignment/Method Class Assignment/Hockey777.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    class Hockey777
+    {
+        //Created or made a method that takes two numbers or intergers as parameters :)
+        public void Hockey(int C, int L)
+        {
+            //Added C + L and then showed the result
+            int result = C + L;
+            Console.WriteLine(C + "+" + L + "=" + result);
+        }
+    }
+}
---
+++ Method Class Assignment/Method Class Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiates Hockey777 class
+            Hockey777 hock = new Hockey777();
+            // Asks the user to provide a number or parameter 
+            Console.WriteLine("Hi please enter a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Thank you");
+            // Asks the user to provide a second number or parameter
+            Console.WriteLine("Please enter a second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Thank you");
+            // Calls the method
+            hock.Hockey(num1, num2);
+            Console.WriteLine("Thank you :) please press Enter to close this");
+            Console.ReadLine();
+        }
+    }
+}
