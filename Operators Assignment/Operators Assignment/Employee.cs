457e07de757d79bb1b69cb294a0b522c277d992c
COBRAHALOLEAFSPEACELOVE7 <129617008+COBRAHALOLEAFSPEACELOVE7@users.noreply.github.com>
2023-10-06 00:22:14 -0400

    Add files via upload

---
+++ Operators Assignment/Operators Assignment/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment
+{
+    class Employee
+    {
+        
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Overloaded == and returned true if emp1's id is equal to emp2's id
+        public static bool operator == (Employee emp1, Employee emp2)
+        {
+            return emp1.id == emp2.id;
+        }
+        // Overloaded == and returned false if emp1's id is not equal to emp2's id
+        public static bool operator != (Employee emp1, Employee emp2)
+        {
+            return emp1.id != emp2.id;
+        }
+    }
+}
+
---
+++ Operators Assignment/Operators Assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Created two employee objects
+            Employee emp1 = new Employee();
+            emp1.id = 1;
+
+            Employee emp2 = new Employee();
+            emp2.id = 1;
+
+            // Created an if statement that will write Equal :) if emp1's id is equal to emp's2 id
+            if (emp1 == emp2)
+            {
+                Console.WriteLine("Equal :)");
+            }
+            Console.ReadLine();
+        }
+    }
+}
