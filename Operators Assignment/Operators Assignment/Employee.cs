83b0cc54afdbe8e9dd9d484c8fa2eb95c15104e8
COBRAHALOLEAFSPEACELOVE7 <129617008+COBRAHALOLEAFSPEACELOVE7@users.noreply.github.com>
2023-10-05 22:46:47 -0400

    Add files via upload

---
+++ Operators Assignment/Operators Assignment/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment
+{
+    class Employee
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static bool operator == (Employee emp1, Employee emp2)
+        {
+            return emp1.id == emp2.id;
+        }
+      public static bool operator != (Employee emp1, Employee emp2)
+        {
+            return emp1.id != emp2.id;
+        }
+    }
+}
+
